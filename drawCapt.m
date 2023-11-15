e03e7c502c25a8eca0c2ec261f9518932c243504
lv150 <145614716+lv150@users.noreply.github.com>
2023-11-15 13:11:27 -0800

    Update drawCapt.m
    capt ++ net

--- drawCapt.m
+++ drawCapt.m
@@ -1,4 +1,4 @@
-function captainGraphics = drawCapt (xCapt , yCapt , thetaCapt , sizeCapt)
+function [captainGraphics, xNet, yNet] = drawCapt (xCapt , yCapt , thetaCapt , sizeCapt)
 
 % In the future, this function will draw the captain at the given
 % position (xCapt , yCapt) , with heading thetaCapt.
@@ -48,22 +48,51 @@ pt13=capt( : , 13);
 pt14=capt( : , 14);
 %pulling points for matrix uses colon to get everything indivdually
 
+
+%newly added points 
+pt15=capt( : , 15);
+pt16=capt( : , 16);
+pt17=capt( : , 17);
+pt18=capt( : , 18);
+pt19=capt( : , 19);
+pt20=capt( : , 20);
+
+%Set the net center
+xNet = pt20(1);
+yNet = pt20(2);
+
+
+
 % Draw the captain and set the return vector of graphics handles.
 %draw line between the points
 %k makes it black
 %captain graphic is a vector (row or column of numbers) adress to graphics of
 %those lines
-captainGraphics(1) = drawLine(pt1 , pt2 , "k");
-captainGraphics(2) = drawLine(pt2 , pt3 , "k");
-captainGraphics(3) = drawLine(pt2 , pt5 , "k");
-captainGraphics(4) = drawLine(pt4 , pt5 , "k");
-captainGraphics(5) = drawLine(pt5 , pt6 , "k");
-captainGraphics(6) = drawLine(pt6 , pt1 , "k");
-captainGraphics(7) = drawLine(pt7 , pt8 , "k");
-captainGraphics(8) = drawLine(pt8 , pt9 , "k");
-captainGraphics(9) = drawLine(pt9 , pt10 , "k");
-captainGraphics(10) = drawLine(pt1 , pt11 , "k");
-captainGraphics(11) = drawLine(pt6 , pt12 , "k");
-captainGraphics(12) = drawLine(pt13 , pt14 , "k");
+captainGraphics(1) = drawLine(pt1 , pt2);
+captainGraphics(2) = drawLine(pt2 , pt3);
+captainGraphics(3) = drawLine(pt2 , pt5);
+captainGraphics(4) = drawLine(pt4 , pt5);
+captainGraphics(5) = drawLine(pt5 , pt6);
+captainGraphics(6) = drawLine(pt6 , pt1);
+captainGraphics(7) = drawLine(pt7 , pt8);
+captainGraphics(8) = drawLine(pt8 , pt9);
+captainGraphics(9) = drawLine(pt9 , pt10);
+captainGraphics(10) = drawLine(pt1 , pt11);
+captainGraphics(11) = drawLine(pt6 , pt12);
+captainGraphics(12) = drawLine(pt13 , pt14);
+captainGraphics(13) = drawLine(pt13 , pt14);
+captainGraphics(14) = drawLine(pt13 , pt15);
+captainGraphics(15) = drawLine(pt15 , pt16);
+captainGraphics(16) = drawLine(pt16 , pt17);
+captainGraphics(17) = drawLine(pt17 , pt18);
+captainGraphics(18) = drawLine(pt18 , pt19);
+captainGraphics(19) = drawLine(pt19 , pt13);
+
+
+
+%set the captain color to black
+for i=1 : length(captainGraphics)
+    set (captainGraphics(i), "color", [0 0 0]);
+  endfor
 
 endfunction
