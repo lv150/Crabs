a6770fffa8ccb74f631670e2241314778f1a6402
lv150 <145614716+lv150@users.noreply.github.com>
2023-11-15 13:18:26 -0800

    Update drawJelly.m
    new jelly

--- drawJelly.m
+++ drawJelly.m
@@ -19,17 +19,29 @@ function jellyHandle = drawJelly (x,y,theta,size)
   pt11=jelly(:,11);
   pt12=jelly(:,12);
   pt13=jelly(:,13);
-  jellyHandle(1)=drawLine(pt1,pt2,'b');
-  jellyHandle(2)=drawLine(pt2,pt3,'y');
-  jellyHandle(3)=drawLine(pt3,pt4,'y');
-  jellyHandle(4)=drawLine(pt4,pt5,'g');
-  jellyHandle(5)=drawLine(pt6,pt7,'y');
-  jellyHandle(6)=drawLine(pt7,pt8,'y');
-  jellyHandle(7)=drawLine(pt8,pt9,'m');
-  jellyHandle(8)=drawLine(pt10,pt11,'m');
-  jellyHandle(9)=drawLine(pt11,pt12,'m');
-  jellyHandle(10)=drawLine(pt12,pt13,'g');
-  jellyHandle(11)=drawLine(pt13,pt5,'m');
-  jellyHandle(12)=drawLine(pt12,pt4,'b');
+  
+  
+  jellyHandle(1)=drawLine(pt1,pt2);
+  jellyHandle(2)=drawLine(pt2,pt3);
+  jellyHandle(3)=drawLine(pt3,pt4);
+  jellyHandle(4)=drawLine(pt4,pt5);
+  jellyHandle(5)=drawLine(pt6,pt7);
+  jellyHandle(6)=drawLine(pt7,pt8);
+  jellyHandle(7)=drawLine(pt8,pt9);
+  jellyHandle(8)=drawLine(pt10,pt11);
+  jellyHandle(9)=drawLine(pt11,pt12);
+  jellyHandle(10)=drawLine(pt12,pt13);
+  jellyHandle(11)=drawLine(pt13,pt5);
+  jellyHandle(12)=drawLine(pt12,pt4);
 
+  %set the jellyfish to a random color 
+  r = rand;
+  g = rand;
+  b = rand;
+  
+  for i=1 : length(jellyHandle)
+    set (jellyHandle(i), "color", [r g b]);
+  endfor
+  
+  
 endfunction
