5b7ce264a8f91df34503ecfed691f2090a1eed05
lv150 <145614716+lv150@users.noreply.github.com>
2023-11-15 13:25:57 -0800

    Update mapCheck.m
    new mapcheck

--- mapCheck.m
+++ mapCheck.m
@@ -1,13 +1,11 @@
-
-
-function inBounds = mapCheck (x, y, width, height, boarder)
+function inBounds = mapCheck (x, y, width, height)
 %This function checks if a point is within the map boundary. 
 %it returns a 1 if it is and a 0 if it is not.
 
-if( x > boarder && x < width-boarder && y > boarder && y < height-boarder)
-    inBounds = 1;
+if(x>width || y>height || x<0 ||y<0)
+    inBounds = false;
   else 
-    inBounds = 0;
+    inBounds = true;
 
 endif
 
