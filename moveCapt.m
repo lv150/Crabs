53798c9b0b294862ffa7a7d091c26dc64f2a9a4b
lv150 <145614716+lv150@users.noreply.github.com>
2023-11-15 13:28:41 -0800

    Update moveCapt.m
    added sprint

--- moveCapt.m
+++ moveCapt.m
@@ -1,16 +1,52 @@
-
 function [xCapt, yCapt, thetaCapt] = moveCapt (cmd, x, y, theta, width, height,size)
 % before = moveCapt (cmd, x, y, theta, width, height, size)
   dtheta = pi/6;
   dstep = 50;
   
+  xCapt = x;
+  yCapt = y;
+  thetaCapt = theta;
+  
+  
+  
+  
  if(cmd == "w") 
   
-  xCapt = x + dstep * cos(theta);
-  yCapt = y + dstep * sin(theta);
-  thetaCapt = theta;
+    xTemp = x + dstep * cos(theta);
+    yTemp = y + dstep * sin(theta);
+    thetaCapt = theta;
+    
+    if(mapCheck(xTemp,yTemp,width,height))
+      xCapt=xTemp;
+      yCapt=yTemp;
+      thetaCapt = theta;
+    else
+      xCapt;
+      yCapt;
+      thetaCapt = theta;
+    endif
+    
   endif 
 
+  if(cmd == "s") 
+  
+    xTemp = x + 100 * cos(theta);
+    yTemp = y + 100 * sin(theta);
+    thetaCapt = theta;
+    
+    if(mapCheck(xTemp,yTemp,width,height))
+      xCapt=xTemp;
+      yCapt=yTemp;
+      thetaCapt = theta;
+    else
+      xCapt;
+      yCapt;
+      thetaCapt = theta;
+    endif
+    
+  endif 
+  
+  
   if(cmd == "d") 
   
   xCapt = x;
@@ -22,9 +58,5 @@ function [xCapt, yCapt, thetaCapt] = moveCapt (cmd, x, y, theta, width, height,s
   xCapt = x;
   yCapt = y; 
   thetaCapt = theta - dtheta;
-    
-  endif
-  
-  %thetaCapt;
-  
+  endif 
 endfunction
